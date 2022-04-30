d0d711c8e4c45428ad11f9d6bb58da493832a0f6
Brindusa <you@example.com>
2022-04-30 15:49:18 +0300

    HTML structure, CSS style & JS functionality

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const $menuBtn = document.querySelector('#menu-btn');
 const $closeMenuBtn = document.querySelector('#close-menu-btn');
 
+const $mediaQuery = window.matchMedia("(min-width: 1200px)");
+
 const $navbar = document.querySelector('#navbar');
 const $mobileNavbar = document.querySelector('#mobile-navbar');
 const $mobileNavLinks = document.querySelectorAll('.mobile-nav__link');
+
+// const $portfolioCardsOdd = document.querySelectorAll('.portfolio-card:nth-child(odd)');
+
+// $portfolioCardsOdd.forEach(oddCard => {
+//     oddCard.classList.add('portfolio-card--odd');
+// })
 
 $menuBtn.addEventListener('click', displayMenu);
 function displayMenu() {
@@ -20,4 +28,13 @@ function closeMenu() {
     $mobileNavbar.classList.remove('mobile-nav--active');
 }
 
+$mediaQuery.addEventListener('change', checkMediaQuery);
+function checkMediaQuery($mediaQuery) {
+    if($mediaQuery.matches) {
+        $closeMenuBtn.style.display="none";
+        $mobileNavbar.classList.remove('mobile-nav--active');
+    } else if(!$mediaQuery.matches && $mobileNavbar.classList.contains('.mobile-nav--active')) {
+        $closeMenuBtn.style.display="block";
+    }
+}
 
